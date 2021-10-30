fbbf45b26f5bbe389affd22e62c348820c970944
KavinTheG <kavin.balag@gmail.com>
2021-10-30 10:14:12 -0400

    Could not retrieve info from Coinranking api.

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '0e86c92ea3mshc73e2445c8070acp110438jsnc4338a0c529d'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/exchanges'
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi; 
